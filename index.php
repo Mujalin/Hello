58c40adae9d17adbeaea2dc5bdc917e2a6e927aa
THANAKORN-BUASRI <59908511+THANAKORN-BUASRI@users.noreply.github.com>
2020-01-22 14:56:33 +0700

    v4
    v4

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 <html>
 <body>
  
-<h1>My first PHP page v3</h1>
+<h1>My first PHP page v4</h1>
  
 <?php
 echo "Hello World! and Thailand <br>";
 echo"Hello Mujalin <br>";
 echo"Hello Mheehehe <br>";
 echo"Hello Suttipong <br>";
+echo"Hello Thanakorn <br>";
 ?>
  
 </body>
