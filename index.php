5fe111330e78baaade736d75bbc0d42b9f7c8b79
Phyajom <49375794+Phyajom@users.noreply.github.com>
2020-01-22 15:01:26 +0700

    v5.
    v5

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
  
-<h1>My first PHP page v4</h1>
+<h1>My first PHP page v5</h1>
  
 <?php
 echo "Hello World! and Thailand <br>";
@@ -10,6 +10,7 @@ echo"Hello Mujalin <br>";
 echo"Hello Mheehehe <br>";
 echo"Hello Suttipong <br>";
 echo"Hello Thanakorn <br>";
+echo"Hello Suppasak <br>";
 ?>
  
 </body>
