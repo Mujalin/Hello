4af56310f874bc89d23ae234639151223edfc4dc
Lapasrada <59908441+Lapasrada@users.noreply.github.com>
2020-01-22 14:52:50 +0700

    v2
    v2

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html>
 <body>
  
-<h1>My first PHP page</h1>
+<h1>My first PHP page v2</h1>
  
 <?php
 echo "Hello World! and Thailand <br>";
 echo"Hello Mujalin <br>";
+echo"Hello Mheehehe <br>";
 ?>
  
 </body>
